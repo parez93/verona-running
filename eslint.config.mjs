f23e45509a7c9456925adb1c40cb75da4a088d47
Federico Parezzan <parez93@hotmail.it>
2025-09-29 17:24:37 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,28 +6,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-    ...compat.config({
-        extends: ['next'],
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+        ],
         rules: {
             "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": "warn",
             "react/no-unescaped-entities": "warn",
-            "@typescript-eslint/no-unused-vars": "warn"
-        }
-    }),
+        },
+    },
 ];
 
 export default eslintConfig;
