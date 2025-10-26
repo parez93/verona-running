f310767927e641d4ed2cc2ea75e4fa47e562df66
Federico Parezzan <parez93@hotmail.it>
2025-10-26 14:50:58 +0100

    fix locale layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,22 +1,19 @@
 import {NextIntlClientProvider} from "next-intl";
 import { ReactNode } from "react";
-import {en} from "@/messages/locales/en";
-import {it} from "@/messages/locales/it";
+import { en } from "@/messages/locales/en";
+import { it } from "@/messages/locales/it";
 
 type Locale = 'it' | 'en';
-
 const allMessages: Record<Locale, typeof it> = { it, en };
 
-/*interface LocaleLayoutProps {
-    children: ReactNode;
-    params: Promise<{ locale: Locale }>;
-}*/
 interface LocaleLayoutProps {
     children: ReactNode;
     params: { locale: Locale };
 }
 
-export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
+// Tip override per evitare il bug su LayoutConfig
+export default async function LocaleLayout(props: any) {
+    const { children, params } = props as LocaleLayoutProps;
     const { locale } = params;
 
     const messages = allMessages[locale];
